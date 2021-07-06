759e594327fc28686c6f577fcb3cb3cd5dfeed4e
elinordin <elin.evelina@live.se>
2021-07-06 10:18:23 +0200

    add pages with export of functional component, test css import

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "../components/layout"
+
+function Blog() {
+    return (
+        <Layout>
+            
+        </Layout>
+    );
+}
+
+export default Blog;
---
+++ src/pages/services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "../components/layout"
+
+function Services() {
+    return (
+        <Layout>
+            
+        </Layout>
+    );
+}
+
+export default Services;
