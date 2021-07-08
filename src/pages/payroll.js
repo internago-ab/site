ed7971ea9b7d1610e23a190ceabfb99539e0155f
elinordin <elin.evelina@live.se>
2021-07-08 13:28:08 +0200

    add grey component with content to services, about and payroll

--- src/pages/payroll.js
+++ src/pages/payroll.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Grey from "../components/grey"
 import Cta from "../components/cta"
 
 function Payroll() {
-
-    return (
-        <Layout>
-            <Seo title="Payroll" />
-            <Cta content="next"/>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Seo title="Payroll" />
+      <Grey>
+        <h1>Internago Payroll Portal</h1>
+        <p>
+          With Internago’s payroll portal you can cut costs and increase
+          efficiency, while still having maximum control over your international
+          payroll.
+          <br />
+          <br />
+          Our international payroll software enables you to manage your payroll
+          and HR in multiple subsidiaries and in one place – online.
+        </p>
+      </Grey>
+      <Cta content="next" />
+    </Layout>
+  )
 }
 
-export default Payroll;
+export default Payroll
