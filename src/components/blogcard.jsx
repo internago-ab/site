37c81d67b573be36e7ca797bca57bd5cf31dce49
elinordin <elin.evelina@live.se>
2021-07-09 11:44:01 +0200

    fix z-index issue on nav and read more link positioning

--- src/components/blogcard.jsx
+++ src/components/blogcard.jsx
@@ -8,14 +8,14 @@ function Blogcard({ post }) {
     const { date, title, description, featuredimage, tags } = post.frontmatter
 
     return (
-        <li className="blog-card">
-            <img src={featuredimage}></img>
+        <li key={post.fields.slug} className="blog-card">
+            <img src={featuredimage} alt="blogpost"></img>
             <div className="text">
                 <Link to={post.fields.slug}><h2>{title}</h2></Link>
                 <p >{date} || {tags.map(tag => <span>{tag} </span>)}</p>
                 <p className="description" dangerouslySetInnerHTML={{__html: description || post.excerpt}}></p>
-                <Link to={post.fields.slug} className="read-more">Read more</Link>
             </div>
+            <Link to={post.fields.slug} className="read-more">Read more</Link>
         </li>
     );
 }
