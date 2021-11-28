e81ea23750ddf597521ab6bac43f34263ef26074
MatildaBjorken <matilda.bjork@hyperisland.se>
2021-11-28 20:43:00 +0100

    nav

--- src/components/countries/facts-and-stats.jsx
+++ src/components/countries/facts-and-stats.jsx
@@ -1,10 +1,19 @@
 import React from "react"
 import "./facts-and-stats.css"
+import { useEffect } from "react"
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 function FactsStats({ description, countryImg }) {
+  useEffect(() => {
+    Aos.init({ duration: 2000 })
+  }, [])
   return (
     <div className="grey-country">
-      <div className="section grey-inside-country imagetext medium facts-stats country-section">
+      <div
+        className="section grey-inside-country imagetext medium facts-stats country-section"
+        data-aos="fade-up"
+      >
         <div>
           <h2 className="heading">Country Facts</h2>
           <p>{description}</p>
